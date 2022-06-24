6ef07a63bc9ea6d7171a041b7fffb069eec06076
George2209 <george.dumitra@hotmail.com>
2022-06-24 13:17:50 +0200

    testing new repository split

--- src/main/java/ro/gdi/canvas/AbstractGameCavan.java
+++ src/main/java/ro/gdi/canvas/AbstractGameCavan.java
@@ -71,7 +71,7 @@ public abstract class AbstractGameCavan extends CavanMovements {
     private XYZMaterial iMaterial = null;
 
     //Light part
-    //TODO: don`t like it as Singleton! To be updated.
+    //TODO: don`t like it as Singleton! To be updated as public constructor.
     private TheSun iTheSun = TheSun.getStaticInstance();
 
     //Textures part
